90bdea51e5f3dddac37a432147a42612c340e70e
aditya <haditya726@gmail.com>
2026-02-27 11:00:15 +0530

    UC1

---
+++ UC-1/PalindromeCheckerApp.java
@@ -0,0 +1,9 @@
+package UC-1;
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Pallindrome Checker App");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully");
+    }
+}
