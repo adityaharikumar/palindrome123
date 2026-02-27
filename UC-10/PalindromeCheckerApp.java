081c8b9f6b151f6772ec5ed445d2c13fad8feb7b
aditya <haditya726@gmail.com>
2026-02-27 09:49:30 +0530

    UC10 - Case-Insensitive & Space-Ignored Palindrome

---
+++ UC-10/PalindromeCheckerApp.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        input = input.toLowerCase();
+        input = input.replaceAll("[^a-z0-9]", "");
+
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+
+        sc.close();
+    }
+}
